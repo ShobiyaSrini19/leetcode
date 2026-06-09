1fa5e303d8330d916454c39173692f9906ce78f2
Shobiya <155611578+ShobiyaSrini19@users.noreply.github.com>
2026-06-09 11:23:08 +0530

    Time: 1 ms (96.01%), Space: 44 MB (23.84%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max=candies[0];
+        int now=0;
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+        for(int i=0;i<candies.length;i++)
+        {
+            now=candies[i]+extraCandies;
+            if(now>=max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+        
+    }
+}
